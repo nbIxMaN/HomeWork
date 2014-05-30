3f1bf7a38bd651af71a0600130aabc3cafc37a67
nbIxMaN <Shulllik@gmail.com>
2014-05-30 16:01:52 +0400

    fixed formatting

--- imageFind/imageFind/Program.fs
+++ imageFind/imageFind/Program.fs
@@ -11,32 +11,39 @@ let url_list = ["http://www.goodfon.ru/catalog/landscapes/";
 
 let rec uniqImageList urlList =
 
-    let imageFind url = 
-        let index (url:string) (x:int) = url.IndexOf ("<img src=", x)
-        let rec imageFound (url:string) x = 
-            let s = index url x
-            if s = -1 then []
-                    else s::imageFound url (s+1)
-        imageFound url 0
+    let getImageList html =
+        let copy position (html:string)  = 
+            let start = html.IndexOf('"', position) + 1
+            let stop = html.IndexOf('"', start + 1) - 1
+            if stop < 0 then html.[start..html.Length - 1]
+                        else html.[start..stop]
 
-    let copy position (url:string) = 
-        let start = url.IndexOf('"', position) + 1
-        let stop = url.IndexOf('"', start + 1) - 1
-        if stop < 0 then url.[start..url.Length - 1]
-                    else url.[start..stop]
+        let imageFind html = 
+            let index (html:string) (x:int) = html.IndexOf ("<img src=", x)
+            let rec imageFound (html:string) x = 
+                let s = index html x
+                if s = -1 then []
+                          else copy s html::imageFound html (s+1)
+            imageFound html 0
+        imageFind html
 
-    let imageList url = Seq.distinct (Seq.map (fun x -> copy x url) (imageFind url))
+        
 
-    Seq.toList (Seq.concat (Seq.filter (fun y -> Seq.length y > 5) (Seq.map (fun x -> imageList x) urlList)))
+    let getUniqImageList = getImageList >> Seq.distinct
+
+    urlList
+    |> Seq.map getUniqImageList
+    |> Seq.filter (fun y -> Seq.length y > 5)
+    |> Seq.concat
+    |> Seq.toList
 
 let getImageList list f=
     let flag = ref false
-    let rec wait() = 
-        if not !flag then System.Threading.Thread.Sleep(100)
-                          wait()
 
     let x = MapCPS.map list WebR.getUrl (fun x -> f (uniqImageList x)
                                                   flag:= true)
-    wait()
+
+    while not !flag do
+        System.Threading.Thread.Sleep(100)
 
 getImageList url_list (printfn "%A")
