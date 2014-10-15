5298c0463378017cdb480f0dc63f30b882e5061d
nbIxMaN <Shulllik@gmail.com>
2014-10-15 22:58:18 +0400

    fix

--- email parse/email parse/Program.fs
+++ email parse/email parse/Program.fs
@@ -6,4 +6,3 @@ let at = "@"
 let lowLevelDomain = "([a-zA-Z0-9]+[\.\-\x5F]?){1,}[a-zA-Z0-9]"
 let hightLevelDomain = "(\.[a-zA-Z]{2}|.aero|.asia|.biz|.cat|.com|.coop|.edu|.gov|.info|.int|.jobs|.mil|.mobi|.museum|.name|.net|.org|.pro|.tel|.travel|.xxx)"
 let isValid email = Regex.IsMatch(email, "^" + login + at + lowLevelDomain + hightLevelDomain + "$")
-printf "%A" (isValid "BIG@BANG.rus")
