688280f39b8da5e50e61d80e2b35747e96d8e88b
nbIxMaN <Shulllik@gmail.com>
2014-10-15 22:56:37 +0400

    Email Parse
    Complete

---
+++ email parse/Test/Library1.fs
@@ -0,0 +1,40 @@
+﻿namespace UnitTest
+
+open emailParse
+open NUnit.Framework
+
+[<TestFixture>]
+module ``Test isValid function`` =
+    [<Test>]
+    let Email1 () =
+        Assert.IsTrue(isValid "ef.w.e.f.w.e.f@wf.w.f.cat")
+    [<Test>]
+    let Email2 () =
+        Assert.IsTrue(isValid "victor.polozov@mail.ru")
+    [<Test>]
+    let Email3 () =
+        Assert.IsTrue(isValid "paints_department@hermitage.museum")
+    [<Test>]
+    let Email4 () =
+        Assert.IsTrue(isValid "my@domain.info")
+    [<Test>]
+    let Email5 () =
+        Assert.IsTrue(isValid "tigr_zelenii.s-serenkim@jivaya.priroda_5.coop")
+    [<Test>]
+    let Email6 () =
+        Assert.IsFalse(isValid "1_.2@google.com")
+    [<Test>]
+    let Email7 () =
+        Assert.IsFalse(isValid "mySQL.@mail.ru")
+    [<Test>]
+    let Email8 () =
+        Assert.IsFalse(isValid "undead'flower@mail.ru")
+    [<Test>]
+    let Email9 () =
+        Assert.IsFalse(isValid "b@d.ru")
+    [<Test>]
+    let Email10 () =
+        Assert.IsFalse(isValid "whereIsMyAt.ru")
+    [<Test>]
+    let Email11 () =
+        Assert.IsFalse(isValid "BIG@BANG.rus")
---
+++ email parse/email parse/Program.fs
@@ -0,0 +1,9 @@
+﻿module emailParse
+open System.Text.RegularExpressions
+
+let login = "([a-zA-Z0-9]+[\.\-\x5F]?){1,}[a-zA-Z0-9]"
+let at = "@"
+let lowLevelDomain = "([a-zA-Z0-9]+[\.\-\x5F]?){1,}[a-zA-Z0-9]"
+let hightLevelDomain = "(\.[a-zA-Z]{2}|.aero|.asia|.biz|.cat|.com|.coop|.edu|.gov|.info|.int|.jobs|.mil|.mobi|.museum|.name|.net|.org|.pro|.tel|.travel|.xxx)"
+let isValid email = Regex.IsMatch(email, "^" + login + at + lowLevelDomain + hightLevelDomain + "$")
+printf "%A" (isValid "BIG@BANG.rus")
